8452878f2eaf9d47f26289f33d7014e86b929afd
BigBenLau <benlau413@gmail.com>
2019-05-29 00:42:28 +0800

    summit main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
+module.exports = function main(str) {
     console.log("Debug Info");
-    return 'Hello World!';
+	var line = [['._.', '...', '._.', '._.', '...', '._.', '._.', '._.', '._.', '._.'], ['|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|'], ['|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|']];
+	var len = str.length;
+	var ans = '';
+	for (var i = 0; i < 3; i++){
+		for (var j = 0; j < len; j++){
+			ans += line[i][str[j]];
+			if(j != len - 1){
+				ans += ' ';
+			}
+			if(j === len - 1){
+				ans += '\n';
+			}
+		}
+	}
+    return ans;
 };
